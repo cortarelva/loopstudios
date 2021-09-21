4ced3a752b76e1d3a73fca97ec63307091610096
cortarelva <45341961+cortarelva@users.noreply.github.com>
2021-09-21 15:52:27 +0100

    99% done

---
+++ loopstudios-landing-page-main/js/main.js
@@ -0,0 +1,7 @@
+
+function openMenu() {
+    document.getElementById('sidenavWindow').style.width = '100%';
+}
+function closeMenu() {
+    document.getElementById('sidenavWindow').style.width = '0';
+}
